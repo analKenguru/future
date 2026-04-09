f8c72147227eb4276822ffedb5e1f0dd24da74c9
Schweppes <alala002@yandex.ru>
2026-04-09 16:34:58 +0300

    0904

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import Header from './Header.js'
 import TabsCollection from './Tabs.js'
+import VideoPlayerCollection from './VideoPLayer.js'
 
 new Header()
 new TabsCollection()
+new VideoPlayerCollection()
