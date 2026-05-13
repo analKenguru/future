d2227da01677959ce00cfbe01508201e9e820c43
Schweppes <alala002@yandex.ru>
2026-05-13 08:39:43 +0300

    1305

--- scripts/Tabs.js
+++ scripts/Tabs.js
@@ -1,6 +1,8 @@
+import BaseComponent from "./BaseComponent.js"
+
 const rootSelector = '[data-js-tabs]'
 
-class Tabs {
+class Tabs extends BaseComponent{
   selectors = {
     root: rootSelector,
     button: '[data-js-tabs-button]',
@@ -18,6 +20,7 @@ class Tabs {
 
 
   constructor(rootElement) {
+    super()
     this.rootElement = rootElement
     this.buttonElements = this.rootElement.querySelectorAll(this.selectors.button)
     this.contentElements = this.rootElement.querySelectorAll(this.selectors.content)
@@ -28,20 +31,6 @@ class Tabs {
     this.bindEvents()
   }
 
-  getProxyState(initialState) {
-    return new Proxy(initialState, {
-      get: (target, prop) => {
-        return target[prop]
-      },
-      set: (target, prop, value) => {
-        target[prop] = value
-
-        this.updateUI()
-
-        return true
-      },
-    })
-  }
 
   updateUI() {
     const { activeTabIndex } = this.state
